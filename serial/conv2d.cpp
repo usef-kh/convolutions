01ea4d6b5b4c24eede4e5e7a507506376bce7f1f
William Martin <wmarti@scc2.bu.edu>
2021-04-16 12:32:15 -0400

    this one works

--- serial/conv2d.cpp
+++ serial/conv2d.cpp
@@ -2,20 +2,14 @@
 #include "conv2d.h"
 using namespace std;
 
-// #define k 3
-// #define n 10
-
-
-void init(double ** res, double ** image, double ** kernel, int n, int k);
-
 int main() {
     
-    int k = 3, n = 10;
+    int k = 5, n = 10;
     double ** kernel = new double * [k];
     double ** image = new double * [n];
-    double ** res = new double * [n];
+    double ** output = new double * [n];
    
-    init(res, image, kernel, n, k);
+    init(output, image, kernel, n, k);
     
     cout << "Kernel" << endl;
     for (int y = 0; y < k; y++) {
@@ -36,12 +30,12 @@ int main() {
     cout << endl << "Result Inital Array" << endl;
     for (int y = 0; y < n; y++) {
         for (int x = 0; x < n; x++) {
-            cout << res[x][y] << " ";
+            cout << output[x][y] << " ";
         }
         cout << endl;
     }
     
-    conv2d(res, image, kernel, n, k);
+    conv2d(output, image, kernel, n, k);
     
         cout << "Kernel" << endl;
     for (int y = 0; y < k; y++) {
@@ -62,7 +56,7 @@ int main() {
     cout << endl << "Result Array" << endl;
     for (int y = 0; y < n; y++) {
         for (int x = 0; x < n; x++) {
-            cout << res[x][y] << " ";
+            cout << output[x][y] << " ";
         }
         cout << endl;
     }
@@ -71,43 +65,3 @@ int main() {
     
     return 0;
 }
-
-
-
-
-
-
-// void init(double ** res, double ** image, double ** kernel, int n, int k){
-    
-//      for (int i = 0; i < k; i++) {
-//         kernel[i] = new double[k];
-//     }
-    
-//     for (int i = 0; i < n; i++) {
-//         image[i] = new double[n];
-//         res[i] = new double[n];
-//     }
-    
-//      //  initialize 2D Kernel
-//     for (int y = 0; y < k; y++) {
-//         for (int x = 0; x < k; x++) {
-//             kernel[x][y] = 1/ 9.0;
-//         }
-//     }
-    
-//     //  initialize 2D images
-//     for (int y = 0; y < n; y++) {
-//         for (int x = 0; x < n; x++) {
-//             if (x % 2 == 0) {
-//                 image[x][y] = 1.0;
-//             } else {
-//                 image[x][y] = 2.0;
-//             }
-            
-//             res[x][y] = 0.0;
-//         }
-//     }
-    
-    
-    
-// }
--- serial/conv2d.h
+++ serial/conv2d.h
@@ -4,26 +4,6 @@
 void conv2d(double ** output, double ** input, double ** kernel, int N, int K);
 void init(double ** res, double ** image, double ** kernel, int n, int k);
 
-// void conv2d(double ** res, double ** image, double ** kernel, int n, int k) {
- 
-//     float sum = 0.0;
-    
-//     for (int y = 1; y < n - 1; y++) {
-//         for (int x = 1; x < n - 1; x++) {
-//             sum = 0.0;
-            
-//             for(int i = -1; i <= 1;i++){
-//                 for(int j = -1; j <=1; j++){
-//                     sum += kernel[j+1][i+1]*image[y - j][x - i];
-//                 }
-//             }
-//             res[y][x] = sum;
-//         }
-//     }
-// }
-
-
-
 void conv2d(double ** output, double ** input, double ** kernel, int N, int K)
 {
 
@@ -51,12 +31,18 @@ void conv2d(double ** output, double ** input, double ** kernel, int N, int K)
 
 void init(double ** output, double ** image, double ** kernel, int n, int k){
     
-    for (int i = 0; i < k; i++)  kernel[i] = new double[k];
+    for (int i = 0; i < k; i++){
+        kernel[i] = new double[k];
+    }
 
-    for (int i = 0; i < n; i++) image[i] = new double[n];
+    for (int i = 0; i < n; i++) {
+        image[i] = new double[n];
+    }
 
-    for(int i = 0; i < n; i ++)  output[i] = new double[n];
-    
+    for(int i = 0; i < n; i++)  {
+        output[i] = new double[n];
+    }
+
      //  initialize 2D Kernel
     for (int y = 0; y < k; y++) {
         for (int x = 0; x < k; x++) {
@@ -80,13 +66,10 @@ void init(double ** output, double ** image, double ** kernel, int n, int k){
         }
     }
     
-    for (int y = 0; y < n - k; y++) {
-        for (int x = 0; x < n - k; x++) {
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
 
             output[x][y] = 0.0;
         }
     }
-    
-    
-    
 }
