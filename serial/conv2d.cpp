c1f1912e4fdd6e1a739037d15acfb18327c16fff
Zhuofa Chen <zfchen@scc1.bu.edu>
2021-04-16 12:18:39 -0400

    rewrite conv2D function, add initialization

--- serial/conv2d.cpp
+++ serial/conv2d.cpp
@@ -2,41 +2,20 @@
 #include "conv2d.h"
 using namespace std;
 
-#define k 3
-#define n 10
+// #define k 3
+// #define n 10
+
+
+void init(double ** res, double ** image, double ** kernel, int n, int k);
 
 int main() {
-
+    
+    int k = 3, n = 10;
     double ** kernel = new double * [k];
     double ** image = new double * [n];
     double ** res = new double * [n];
-    
-    for (int i = 0; i < k; i++) {
-        kernel[i] = new double[k];
-    }
-    
-    for (int i = 0; i < n; i++) {
-        image[i] = new double[n];
-        res[i] = new double[n];
-    }
-    
-    for (int y = 0; y < k; y++) {
-        for (int x = 0; x < k; x++) {
-            kernel[x][y] = 1/ 9.0;
-        }
-    }
-    
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < n; x++) {
-            if (x % 2 == 0) {
-                image[x][y] = 1.0;
-            } else {
-                image[x][y] = 2.0;
-            }
-            
-            res[x][y] = 0.0;
-        }
-    }
+   
+    init(res, image, kernel, n, k);
     
     cout << "Kernel" << endl;
     for (int y = 0; y < k; y++) {
@@ -92,3 +71,43 @@ int main() {
     
     return 0;
 }
+
+
+
+
+
+
+// void init(double ** res, double ** image, double ** kernel, int n, int k){
+    
+//      for (int i = 0; i < k; i++) {
+//         kernel[i] = new double[k];
+//     }
+    
+//     for (int i = 0; i < n; i++) {
+//         image[i] = new double[n];
+//         res[i] = new double[n];
+//     }
+    
+//      //  initialize 2D Kernel
+//     for (int y = 0; y < k; y++) {
+//         for (int x = 0; x < k; x++) {
+//             kernel[x][y] = 1/ 9.0;
+//         }
+//     }
+    
+//     //  initialize 2D images
+//     for (int y = 0; y < n; y++) {
+//         for (int x = 0; x < n; x++) {
+//             if (x % 2 == 0) {
+//                 image[x][y] = 1.0;
+//             } else {
+//                 image[x][y] = 2.0;
+//             }
+            
+//             res[x][y] = 0.0;
+//         }
+//     }
+    
+    
+    
+// }
--- serial/conv2d.h
+++ serial/conv2d.h
@@ -1,20 +1,92 @@
 #pragma once
-void conv2d(double ** res, double ** image, double ** kernel, int n, int k);
 
-void conv2d(double ** res, double ** image, double ** kernel, int n, int k) {
+
+void conv2d(double ** output, double ** input, double ** kernel, int N, int K);
+void init(double ** res, double ** image, double ** kernel, int n, int k);
+
+// void conv2d(double ** res, double ** image, double ** kernel, int n, int k) {
  
-    float sum = 0.0;
+//     float sum = 0.0;
     
-    for (int y = 1; y < n - 1; y++) {
-        for (int x = 1; x < n - 1; x++) {
-            sum = 0.0;
+//     for (int y = 1; y < n - 1; y++) {
+//         for (int x = 1; x < n - 1; x++) {
+//             sum = 0.0;
             
-            for(int i = -1; i <= 1;i++){
-                for(int j = -1; j <=1; j++){
-                    sum += kernel[j+1][i+1]*image[y - j][x - i];
-                }
+//             for(int i = -1; i <= 1;i++){
+//                 for(int j = -1; j <=1; j++){
+//                     sum += kernel[j+1][i+1]*image[y - j][x - i];
+//                 }
+//             }
+//             res[y][x] = sum;
+//         }
+//     }
+// }
+
+
+
+void conv2d(double ** output, double ** input, double ** kernel, int N, int K)
+{
+
+	// Fill output matrix: rows and columns are i and j respectively
+	for (int i = 0; i < N - K; i++)
+	{
+		for (int j = 0; j <  N - K; j++)
+		{
+		    double convolute = 0;
+			// Kernel rows and columns are k and l respectively
+			for (int k = 0; k < K; k++)
+			{
+				for (int l = 0; l < K; l++)
+				{
+					// Convolute here.
+					convolute += kernel[k][l] * input[i + k][j + l];
+				}
+			}
+			output[i][j] = convolute; // Add result to output matrix.
+		}
+	}
+}
+
+
+
+void init(double ** output, double ** image, double ** kernel, int n, int k){
+    
+    for (int i = 0; i < k; i++)  kernel[i] = new double[k];
+
+    for (int i = 0; i < n; i++) image[i] = new double[n];
+
+    for(int i = 0; i < n; i ++)  output[i] = new double[n];
+    
+     //  initialize 2D Kernel
+    for (int y = 0; y < k; y++) {
+        for (int x = 0; x < k; x++) {
+           if (x % 2 == 0) {
+                kernel[x][y] = 1.0/9;
+            } else {
+                kernel[x][y] = 2.0/9;
             }
-            res[y][x] = sum;
+            
         }
     }
+    
+    //  initialize 2D images
+    for (int y = 0; y < n; y++) {
+        for (int x = 0; x < n; x++) {
+            if (x % 2 == 0) {
+                image[x][y] = 1.0;
+            } else {
+                image[x][y] = 2.0;
+            }
+        }
+    }
+    
+    for (int y = 0; y < n - k; y++) {
+        for (int x = 0; x < n - k; x++) {
+
+            output[x][y] = 0.0;
+        }
+    }
+    
+    
+    
 }
